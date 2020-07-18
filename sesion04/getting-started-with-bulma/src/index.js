cd10bbfe45bfaa2b7b796642c0cb1f37572371b5
Jorge De Lara <mentorlinkr12@nube.unadmexico.mx>
2020-07-18 10:12:01 -0600

    Adding personalization bulma

--- sesion04/getting-started-with-bulma/src/index.js
+++ sesion04/getting-started-with-bulma/src/index.js
@@ -1,5 +1,6 @@
 import './sass/mybulma.scss' //Import is readeable by webpack, if we have some sintax unknown, we have to use babel
 import './sass/styles.scss'//We import all sass extensions for its use
+//import 'bulma/bulma.sass no use this because this is not funtional to personalize'
 import '@fortawesome/fontawesome-free/js/all'//We import fontawesome framework
 
 const sayHello = 'Getting started with bulma' //using const to add a text into variable
