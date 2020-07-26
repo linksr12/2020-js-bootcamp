368e2941611d8c845a42496979ed9b1e1c7514db
Jorge De Lara <mentorlinkr12@nube.unadmexico.mx>
2020-07-25 18:00:17 -0600

    Login finished

--- sesion06/bulma-dashboard/src/index.js
+++ sesion06/bulma-dashboard/src/index.js
@@ -3,5 +3,5 @@ import './sass/styles.scss'//We import all sass extensions for its use
 //import 'bulma/bulma.sass no use this because this is not funtional to personalize'
 import '@fortawesome/fontawesome-free/js/all'//We import fontawesome framework
 
-const sayHello = 'Getting started with bulma' //using const to add a text into variable
+const sayHello = 'bulma dashboard' //using const to add a text into variable
 console.log(sayHello)//Shows in console the result.
